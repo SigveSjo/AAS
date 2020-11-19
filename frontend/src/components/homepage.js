c8c2d9bc90d948ec096fb24a5c803408270a3c2f
AndreasArnholm <andreas@arnholm.org>
2020-11-19 14:29:51 +0100

    wip frontend

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Paper, withStyles, Grid, TextField, Button } from '@material-ui/core';
-import { SportsEsports} from '@material-ui/icons'
+import { SportsEsports, ArrowUpwardRounded, ArrowDownwardRounded} from '@material-ui/icons'
 import axios from 'axios';
 
 const styles = theme => ({
@@ -11,8 +11,10 @@ const styles = theme => ({
         padding: theme.spacing(1)
     }
 });
+const API_URL = "http://127.0.0.1:8000/api/commands/"
 
 var value = ""
+var speed = 0.1
 
 class Homepage extends Component {
 
@@ -20,10 +22,46 @@ class Homepage extends Component {
         value = event.target.value
       };
 
-    buttonClicked(){
-        console.log("Im clicked XD ", value)
+    handleOnChangeSpeed = event => {
+        speed = event.target.value
+      };
 
-        axios.post("http://127.0.0.1:8000/api/commands/", { "command" : value})
+    buttonClicked(){
+        console.log("Command sent!", value)
+
+        axios.post(API_URL, { "command" : value})
+
+        console.log(Date.now());
+    }
+
+    moveKMR(){
+
+        var direction = "up"
+        var vector = null
+
+        console.log("FIRE")
+
+        if(direction === "up"){
+            vector = " 1 0 0"
+        }
+        if(direction === "down"){
+            vector = " -1 0 0"
+        }
+        if(direction === "left"){
+            vector = " 0 1 0"
+        }
+        if(direction === "right"){
+            vector = " 0 -1 0"
+        }
+        if(direction === "clockwise"){
+            vector = " 0 0 1"
+        }
+        if(direction === "counter-clockwise"){
+            vector = " 0 0 -1"
+        }
+        
+        axios.post(API_URL, { "command" : "kmr:" + speed + vector})
+
     }
 
     render() {
@@ -43,6 +81,21 @@ class Homepage extends Component {
                     </Grid>
                     <Grid container justify="center" style={{ marginTop: '10px' }}>
                         <Button onClick={this.buttonClicked} variant="outlined" color="primary" style={{ textTransform: "none" }}>Send</Button>
+                    </Grid>
+                </div>
+                <div className={classes.margin}>
+                    <Grid container spacing={8} alignItems="flex-end">
+                        <Grid item>
+                            <SportsEsports />
+                        </Grid>
+                        <Grid item md={true} sm={true} xs={true}>
+                            <TextField onChange={this.handleOnChangeSpeed} id="speed" type="string" fullWidth autoFocus/>
+                        </Grid>
+                    </Grid>
+                    <Grid container alignItems="center" justify="space-between">
+                    </Grid>
+                    <Grid container justify="center" style={{ marginTop: '10px' }}>
+                        <Button onClick={this.moveKMR()} variant="outlined" color="primary" style={{ textTransform: "none" }}><ArrowUpwardRounded /></Button>
                     </Grid>
                 </div>
             </Paper>
