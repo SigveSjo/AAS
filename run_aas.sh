38bed3bff9a873df3cb0bd90e0e6e13c2bbd7fd2
AndreasArnholm <andreas@arnholm.org>
2021-02-17 11:36:50 +0100

    changed config.json - OPC UA location

--- run_aas.sh
+++ run_aas.sh
@@ -7,9 +7,9 @@ opcua_ip=$3
 api="http://$internal_interface_ip/api/"
 ws="ws://$internal_interface_ip/api/"
 
-sudo cat config.json | jq '.OPCUA_URL = $v' --arg v $opcua_ip | sponge config.json
+sudo cat internal_interface/internal_interface/config.json | jq '.OPCUA_URL = $v' --arg v $opcua_ip | sponge internal_interface/internal_interface/config.json
 sudo cat frontend/src/config.json | jq '.API_URL = $v' --arg v $api | sponge frontend/src/config.json
 sudo cat frontend/src/config.json | jq '.WS_URL = $v' --arg v $ws | sponge frontend/src/config.json
 
-gnome-terminal -- python3.8 internal_interface/internal_interface/manage.py runserver $internal_interface_ip
+( cd internal_interface/internal_interface && gnome-terminal -- python3.8 manage.py runserver $internal_interface_ip )
 ( cd frontend && gnome-terminal -- npm run start HOST=$frontend_ip )
