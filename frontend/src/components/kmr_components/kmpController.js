2e8a23b96af10e5437f5f26a02fb317e5bdf541b
mathiasnh <mathias.neslow96@gmail.com>
2021-04-20 16:02:05 +0200

    add keyboard controls (from old branch)

--- frontend/src/components/kmr_components/kmpController.js
+++ frontend/src/components/kmr_components/kmpController.js
@@ -1,8 +1,6 @@
-import { useState, useEffect } from 'react';
-import { Paper, withStyles, Grid, Button, Slider } from '@material-ui/core';
+import { useState, useEffect } from 'react'
+import { Paper, withStyles, Grid, Button, Slider } from '@material-ui/core'
 import { ArrowUpwardRounded, ArrowDownwardRounded, ArrowBack, ArrowForward, RotateLeft, RotateRight} from '@material-ui/icons'
-import axios from 'axios';
-import configs from '../../config.json'
 
 const styles = theme => ({
     margin: {
@@ -11,7 +9,7 @@ const styles = theme => ({
     padding: {
         padding: theme.spacing(1)
     }
-});
+})
 
 function KMPController(props) {
     const [speed, setSpeed] = useState(0.1)
@@ -23,7 +21,7 @@ function KMPController(props) {
 
     const handleOnChangeSpeed = (event, newValue) => {
         setSpeed(newValue)
-    };
+    }
 
     const moveKMP = (direction) => {
         var vector = null
@@ -52,8 +50,83 @@ function KMPController(props) {
         
         props.ws.emit('command', { "command" : "kmp:" + speed + vector })
     }
+    const defaultColor = "white"
+    const pressColor = "gray"
 
-    const { classes } = props;
+    const [upColor, setUpColor] = useState(defaultColor)
+    const [downColor, setDownColor] = useState(defaultColor)
+    const [leftColor, setLeftColor] = useState(defaultColor)
+    const [rightColor, setRightColor] = useState(defaultColor)
+    const [counterClockwiseColor, setCounterClockwiseColor] = useState(defaultColor)
+    const [clockwiseColor, setClockwiseColor] = useState(defaultColor)
+
+    document.onkeydown = function(event) {
+        if (event.key == 'ArrowUp' && upColor==defaultColor) {
+            moveKMP("up")
+            console.log("ArrowUp pressed")
+            setUpColor(pressColor)
+        }
+        if (event.key == 'ArrowDown' && downColor==defaultColor) {
+            moveKMP("down")
+            console.log("ArrowDown pressed")
+            setDownColor(pressColor)
+        }
+        if (event.key == 'ArrowLeft' && leftColor==defaultColor) {
+            moveKMP("left")
+            console.log("ArrowLeft pressed")
+            setLeftColor(pressColor)
+        }
+        if (event.key == 'ArrowRight' && rightColor==defaultColor) {
+            moveKMP("right")
+            console.log("ArrowRight pressed")
+            setRightColor(pressColor)
+        }
+        if (event.key == 'o' && counterClockwiseColor==defaultColor) {
+            moveKMP("counter-clockwise")
+            console.log("CounterClockwise pressed")
+            setCounterClockwiseColor(pressColor)
+        }
+        if (event.key == 'p' && clockwiseColor==defaultColor) {
+            moveKMP("clockwise")
+            console.log("clockwise pressed")
+            setClockwiseColor(pressColor)
+        }
+    }
+
+    document.onkeyup = function(event) {
+        if (event.key == 'ArrowUp') {
+            moveKMP("stop")
+            console.log("ArrowUp released")
+            setUpColor(defaultColor)
+        }
+        if (event.key == 'ArrowDown') {
+            moveKMP("stop")
+            console.log("ArrowDown released")
+            setDownColor(defaultColor)
+        }
+        if (event.key == 'ArrowLeft') {
+            moveKMP("stop")
+            console.log("ArrowLeft released")
+            setLeftColor(defaultColor)
+        }
+        if (event.key == 'ArrowRight') {
+            moveKMP("stop")
+            console.log("ArrowRight released")
+            setRightColor(defaultColor)
+        }
+        if (event.key == 'o') {
+            moveKMP("stop")
+            console.log("CounterClockwise released")
+            setCounterClockwiseColor(defaultColor)
+        }
+        if (event.key == 'p') {
+            moveKMP("stop")
+            console.log("clockwise released")
+            setClockwiseColor(defaultColor)
+        }
+    }
+
+    const { classes } = props
     return (
         <Paper className={classes.padding}>
             <Grid container justify="center">
@@ -71,30 +144,30 @@ function KMPController(props) {
                 disabled={!enabled} 
                 />
                 <Grid container justify="center" style={{ marginTop: '10px' }}>
-                    <Button disabled={!enabled} onMouseUp ={() => moveKMP('stop')} onMouseDown ={() => moveKMP('up')} variant="outlined" color="primary" style={{ textTransform: "none" }}><ArrowUpwardRounded /></Button>
+                    <Button disabled={!enabled} onMouseUp ={() => moveKMP('stop')} onMouseDown ={() => moveKMP('up')} variant="outlined" color="primary" style={{ textTransform: "none", backgroundColor: upColor}}><ArrowUpwardRounded /></Button>
                 </Grid>
                 <Grid container justify="center" style={{ marginTop: '10px' }}>
                     <Grid>
-                        <Button disabled={!enabled} onMouseUp ={() => moveKMP('stop')} onMouseDown ={() => moveKMP('left')} variant="outlined" color="primary" style={{ textTransform: "none" }}><ArrowBack /></Button>
+                        <Button disabled={!enabled} onMouseUp ={() => moveKMP('stop')} onMouseDown ={() => moveKMP('left')} variant="outlined" color="primary" style={{ textTransform: "none", backgroundColor: leftColor}}><ArrowBack /></Button>
                     </Grid>
                     <Grid>
-                        <Button disabled={!enabled} onMouseUp ={() => moveKMP('stop')} onMouseDown ={() => moveKMP('down')} variant="outlined" color="primary" style={{ textTransform: "none" }}><ArrowDownwardRounded /></Button>
+                        <Button disabled={!enabled} onMouseUp ={() => moveKMP('stop')} onMouseDown ={() => moveKMP('down')} variant="outlined" color="primary" style={{ textTransform: "none", backgroundColor: downColor}}><ArrowDownwardRounded /></Button>
                     </Grid>
                     <Grid>
-                        <Button disabled={!enabled} onMouseUp ={() => moveKMP('stop')} onMouseDown ={() => moveKMP('right')} variant="outlined" color="primary" style={{ textTransform: "none" }}><ArrowForward/></Button>
+                        <Button disabled={!enabled} onMouseUp ={() => moveKMP('stop')} onMouseDown ={() => moveKMP('right')} variant="outlined" color="primary" style={{ textTransform: "none", backgroundColor: rightColor}}><ArrowForward/></Button>
                     </Grid>
                 </Grid>
                 <Grid container justify="center" style={{ marginTop: '10px' }}>
                     <Grid>
-                        <Button disabled={!enabled} onMouseUp ={() => moveKMP('stop')} onMouseDown ={() => moveKMP('counter-clockwise')} variant="outlined" color="primary" style={{ textTransform: "none" }}><RotateLeft /></Button>
+                        <Button disabled={!enabled} onMouseUp ={() => moveKMP('stop')} onMouseDown ={() => moveKMP('counter-clockwise')} variant="outlined" color="primary" style={{ textTransform: "none", backgroundColor: counterClockwiseColor}}><RotateLeft /></Button>
                     </Grid>
                     <Grid>
-                        <Button disabled={!enabled} onMouseUp ={() => moveKMP('stop')} onMouseDown ={() => moveKMP('clockwise')} variant="outlined" color="primary" style={{ textTransform: "none" }}><RotateRight /></Button>
+                        <Button disabled={!enabled} onMouseUp ={() => moveKMP('stop')} onMouseDown ={() => moveKMP('clockwise')} variant="outlined" color="primary" style={{ textTransform: "none", backgroundColor: clockwiseColor}}><RotateRight /></Button>
                     </Grid>
                 </Grid>
             </div>
         </Paper>
-    );
+    )
     
 }
 
