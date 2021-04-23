e6f4ca5324e0a9e3511f48401ae22a62c6b18b9e
Mathias Henriksen <mnh@vitalthings.com>
2021-04-23 13:23:12 +0200

    Update robot component page UI (better video feed position)

--- frontend/src/components/robotComponents.js
+++ frontend/src/components/robotComponents.js
@@ -4,39 +4,17 @@ import CircularProgress from '@material-ui/core/CircularProgress'
 import KMRGeneralCommands from './kmr_components/kmrGeneralCommands'
 import KMPController from './kmr_components/kmpController'
 import LBRController from './kmr_components/lbrController'
+import VideoComponent from './video'
 
 const styles = theme => ({
     root: {
       flexGrow: 1,
       padding: theme.spacing(2,4,3),
       borderRadius: 2,
-    },
-    loading: {
-        position: 'absolute',
-    },
-    imageLoading: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    progress: {
-        color: '#f37a00',
     }
   })
 
 function RobotComponents(props) {
-    const [loading, setLoading] = useState(false)
-
-    const handleLoad = (e) => {
-        setLoading(false)
-    }
-
-    const setSrc = (e) => {
-        setLoading(true)
-        setTimeout(() => { 
-            e.target.src=localStorage.getItem('camera_stream_url_' + props.rid)
-        }, 1500);
-    }
     
     const { classes } = props
     return (
@@ -47,13 +25,20 @@ function RobotComponents(props) {
                     props.name === 'KMR' && 
                     <Grid container justify="center" spacing={3}>
                         <Grid item>
-                            <KMRGeneralCommands ws={props.ws}/>
-                        </Grid>
-                        <Grid item>
                             <KMPController ws={props.ws} status={props.state.components.kmp} rid={props.rid}/>
                         </Grid>
                         <Grid item>
                             <LBRController ws={props.ws} status={props.state.components.lbr} rid={props.rid}/>
+                        </Grid>
+                        <Grid item>
+                            <VideoComponent 
+                                rid={props.rid}
+                                state={props.state}
+                                handleCameraEvent={props.handleCameraEvent}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <KMRGeneralCommands ws={props.ws}/>
                         </Grid>
                     </Grid>
                 }
@@ -62,34 +47,36 @@ function RobotComponents(props) {
                     // (INSERT ROBOT HERE) components
                 }
             </Grid>
-            <Grid item>
-
-                <Grid>
-                    <Button onClick={props.handleCameraEvent}>
-                        {props.state.cameraButton}
-                    </Button>
-                </Grid>
-                {
-                    (props.state.cameraButton.localeCompare("Stop") == 0) &&
-                    <Grid> 
-                        <div className={classes.imageLoading}>
-                            {
-                                loading &&
-                                <div className={classes.loading}>
-                                    <CircularProgress 
-                                        className={classes.progress}
-                                        size={100}
-                                        thickness={4}
-                                    />
-                                </div>
-                            }
-                            <div className={classes.image}>
-                                <img src={props.state.streamURL || localStorage.getItem('camera_stream_url_' + props.rid)} width="200px" height="200px" onError={(e)=>{setSrc(e)}} onLoad={()=>{handleLoad()}}/>
-                            </div>
-                        </div>
+            {
+                /*
+                <Grid item>
+    
+                    <Grid>
+                        <Button onClick={props.handleCameraEvent}>
+                            {props.state.cameraButton}
+                        </Button>
                     </Grid>
-                }
-            </Grid>
+                    {
+                        (props.state.cameraButton.localeCompare("Stop") == 0) &&
+                        <Grid> 
+                            <div className={classes.imageLoading}>
+                                {
+                                    loading &&
+                                    <div className={classes.loading}>
+                                        <CircularProgress 
+                                            className={classes.progress}
+                                        />
+                                    </div>
+                                }
+                                <div className={classes.image}>
+                                    <img src={props.state.streamURL || localStorage.getItem('camera_stream_url_' + props.rid)} width="200px" height="200px" onError={(e)=>{setSrc(e)}} onLoad={()=>{handleLoad()}}/>
+                                </div>
+                            </div>
+                        </Grid>
+                    }
+                </Grid>
+                */
+            }
         </Grid>
     )
 }
---
+++ frontend/src/components/video.js
@@ -0,0 +1,114 @@
+import { useState } from 'react';
+import { Paper, withStyles, Button, Grow, Grid, IconButton } from '@material-ui/core'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import Tooltip from '@material-ui/core/Tooltip'
+import Videocam from '@material-ui/icons/Videocam'
+import VideocamOff from '@material-ui/icons/VideocamOff'
+
+const styles = theme => ({
+    padding: {
+        padding: theme.spacing(1),
+    },
+    loading: {
+        padding: theme.spacing(1),
+    },
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    progress: {
+        color: '#f37a00',
+    }
+  })
+
+function VideoComponent(props) {
+    const [loading, setLoading] = useState(false)
+    const [cameraStatus, setCameraStatus] = useState(false)
+
+    const handleLoad = (e) => {
+        setLoading(false)
+    }
+    
+    const setSrc = (e) => {
+        //e.target.style.display='none'
+        setLoading(true)
+        setTimeout(() => { 
+            e.target.src=localStorage.getItem('camera_stream_url_' + props.rid)
+        }, 1500);
+    }
+
+    const handleImageEvent = () => {
+        setCameraStatus(!cameraStatus)
+    }
+    
+    const { classes } = props
+    return (
+        <Paper className={classes.padding}>
+            <Button onClick={props.handleCameraEvent} variant="outlined" color="primary" style={{ textTransform: "none" }}>
+                {props.state.cameraButton} video feed
+            </Button>
+                <IconButton onClick={handleImageEvent}>
+                    <Tooltip title={cameraStatus ? "Close video window" : "Open video window"}>
+                    {
+                        cameraStatus ?
+                        <VideocamOff fontSize="large" color="primary"/>
+                        :
+                        <Videocam fontSize="large" color="primary"/>
+                    }
+                    </Tooltip>
+                </IconButton>
+            {
+                (props.state.cameraButton.localeCompare("Stop") == 0) &&
+                <div className={classes.container}>
+                    {
+                        loading &&
+                        <div className={classes.loading}>
+                            <CircularProgress 
+                                className={classes.progress}
+                                size={120}
+                                thickness={2}
+                                />
+                        </div>
+                    }
+                    <div style={{display: loading ? "none" : "block"}}>
+                        <img src={props.state.streamURL || localStorage.getItem('camera_stream_url_' + props.rid)} onError={(e)=>{setSrc(e)}} onLoad={(e)=>{handleLoad(e)}}/>
+                    </div>
+                </div>
+            }
+            {
+            /*
+            <Grid container>
+                <Grid item>
+                    <Button onClick={props.handleCameraEvent}>
+                        {props.state.cameraButton}
+                    </Button>
+                </Grid>
+                <Grid item>
+                    {
+                        (props.state.cameraButton.localeCompare("Stop") == 0) &&
+                        <Grid container justify="flex-start" alignItems="flex-start">
+                            <Grid item className={classes.loading}>
+                                {
+                                    loading &&
+                                    <CircularProgress 
+                                        className={classes.progress}
+                                        size={150}
+                                        thickness={4}
+                                    />
+                                }
+                            </Grid>
+                            <Grid item className={classes.image} justify="flex-start">
+                                <img src={props.state.streamURL || localStorage.getItem('camera_stream_url_' + props.rid)} onError={(e)=>{setSrc(e)}} onLoad={()=>{handleLoad()}}/>
+                            </Grid>
+                        </Grid>
+                    }
+                </Grid>
+            </Grid>
+            */
+            }
+        </Paper>
+    )
+}
+
+export default withStyles(styles)(VideoComponent)
