296ce6ab3074c212b5457cec46a9319af9116659
Diaa <ditek@users.noreply.github.com>
2022-10-18 14:28:50 +0200

    Fix references to old backend location

--- run_aas.sh
+++ run_aas.sh
@@ -10,9 +10,9 @@ api="http://$internal_interface_ip/"
 
 internal_interface_split=(${internal_interface_ip//:/ })
 
-sudo cat internal_interface_flask/config.json | jq '.OPCUA_URL = $v' --arg v $opcua_ip | sponge internal_interface_flask/config.json
-sudo cat internal_interface_flask/config.json | jq '.SERVER_URL = $v' --arg v $internal_interface_ip | sponge internal_interface_flask/config.json
+sudo cat internal_interface/config.json | jq '.OPCUA_URL = $v' --arg v $opcua_ip | sponge internal_interface/config.json
+sudo cat internal_interface/config.json | jq '.SERVER_URL = $v' --arg v $internal_interface_ip | sponge internal_interface/config.json
 sudo cat frontend/src/config.json | jq '.API_URL = $v' --arg v $api | sponge frontend/src/config.json
 
-( cd internal_interface_flask && gnome-terminal -- python3 internal_interface.py )
+( cd internal_interface && gnome-terminal -- python3 internal_interface.py )
 ( cd frontend && gnome-terminal -- npm run start HOST=$frontend_ip )
