8029ecda13781a3936c5660a14c0803ac8b575b9
Diaa <ditek@users.noreply.github.com>
2023-02-08 02:26:57 +0100

    Modify the run script to run on OpenShift

--- run_aas_headless.sh
+++ run_aas_headless.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # E.g for local: bash run_aas.sh 127.0.0.1 127.0.0.1:8000 0.0.0.0:4841
+# source /root/.bashrc
 
 frontend_ip=$1
 internal_interface_ip=$2
@@ -10,9 +11,9 @@ api="http://$internal_interface_ip/"
 
 internal_interface_split=(${internal_interface_ip//:/ })
 
-sudo cat internal_interface/config.json | jq '.OPCUA_URL = $v' --arg v $opcua_ip | sponge internal_interface/config.json
-sudo cat internal_interface/config.json | jq '.SERVER_URL = $v' --arg v $internal_interface_ip | sponge internal_interface/config.json
-sudo cat frontend/src/config.json | jq '.API_URL = $v' --arg v $api | sponge frontend/src/config.json
+cat internal_interface/config.json | jq '.OPCUA_URL = $v' --arg v $opcua_ip | sponge internal_interface/config.json
+cat internal_interface/config.json | jq '.SERVER_URL = $v' --arg v $internal_interface_ip | sponge internal_interface/config.json
+cat frontend/src/config.json | jq '.API_URL = $v' --arg v $api | sponge frontend/src/config.json
 
 cd internal_interface && python3 internal_interface.py &
-cd frontend && npm run start HOST=$frontend_ip
+cd frontend && /root/.nvm/versions/node/v14.21.1/bin/npm run start HOST=$frontend_ip
