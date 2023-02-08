ffb6bfbe685e68a44fee37cefbfafd5c513e6df1
Diaa <ditek@users.noreply.github.com>
2023-02-08 02:20:35 +0100

    Update mac script (not tested)

--- run_aas_mac.sh
+++ run_aas_mac.sh
@@ -13,10 +13,10 @@ internal_interface_split=(${internal_interface_ip//:/ })
 ip=${internal_interface_split[0]}
 port=${internal_interface_split[1]}
 
-sudo cat internal_interface/internal_interface/config.json | jq '.OPCUA_URL = $v' --arg v $opcua_ip | sponge internal_interface/internal_interface/config.json
+sudo cat internal_interface/config.json | jq '.OPCUA_URL = $v' --arg v $opcua_ip | sponge internal_interface/config.json
 sudo cat frontend/src/config.json | jq '.API_URL = $v' --arg v $api | sponge frontend/src/config.json
 sudo cat frontend/src/config.json | jq '.WS_URL = $v' --arg v $ws | sponge frontend/src/config.json
 
-osascript -e 'tell application "Terminal" to do script "( cd git/AAS/internal_interface/internal_interface && export DJANGO_SETTINGS_MODULE=internal_interface.settings && daphne -b '$ip' -p '$port' internal_interface.asgi:application )"'
+osascript -e 'tell application "Terminal" to do script "( cd git/AAS/internal_interface && export DJANGO_SETTINGS_MODULE=internal_interface.settings && daphne -b '$ip' -p '$port' internal_interface.asgi:application )"'
 
-osascript -e 'tell application "Terminal" to do script "( cd git/AAS/frontend && npm run start HOST=$frontend_ip )"'
+# osascript -e 'tell application "Terminal" to do script "( cd git/AAS/frontend && npm run start HOST=$frontend_ip )"'
